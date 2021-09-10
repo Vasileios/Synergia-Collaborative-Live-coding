0878240ba9569de6226f533aaba2ee9b21e8c445
vasilis <agios.mur@gmail.com>
2021-09-10 17:47:09 +0300

    update

--- shell_scripts/2_open_vpn_auto_configure.sh
+++ shell_scripts/2_open_vpn_auto_configure.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 #high_sierra_open_vpn_downloaded_form_the_website
-open "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" ~/Downloads/rpi.ovpn
+open "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" ~/Documents/Projects/GitHub_Vasileios/Collaborative-Live-coding/ovpns/usr1.ovpn
--- shell_scripts/3_after_vpn.sh
+++ shell_scripts/3_after_vpn.sh
@@ -1,22 +1,22 @@
 #! /bin/sh
 
 osascript -e 'tell app "Terminal"
-do script "ssh -o PreferredAuthentications=raspberry pi@192.168.1.51"
+do script "ssh -o PreferredAuthentications=raspberry user1@192.168.1.51"
 end tell'
 
 
-#spawn ssh pi@192.168.1.51
-#expect "raspberry"
-#send "raspberry\r"
+#spawn ssh user@192.168.1.51
+#expect "user1"
+#send "user1\r"
 #interact
 
 #! /bin/sh
 #osascript -e 'tell app "Terminal"
-#do script "spawn ssh pi@192.168.1.51 expect "raspberry" send "raspberry\r" interact"
+#do script "spawn ssh user1@192.168.1.51 expect "user1" send "user1\r" interact"
 #end tell'
 #osascript -e 'tell app "Terminal"
 #do script "spawn ssh pi@192.168.1.51
-#expect "raspberry"
-#send "raspberry\r"
+#expect "user1"
+#send "user1\r"
 #interact"
 #end tell'
