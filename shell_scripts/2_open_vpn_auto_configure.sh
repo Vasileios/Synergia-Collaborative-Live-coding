4735b552e3bca25ee73a6997a75f75c9f0a6da78
vasilis <agios.mur@gmail.com>
2021-09-10 23:46:15 +0300

    update openvpn access

--- shell_scripts/2_open_vpn_auto_configure.sh
+++ shell_scripts/2_open_vpn_auto_configure.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
 #high_sierra_open_vpn_downloaded_form_the_website
-open "/Applications/OpenVPN Connect/OpenVPN Connect.app/Contents/MacOS/OpenVPN Connect" ~/Documents/Projects/GitHub_Vasileios/Collaborative-Live-coding/ovpns/usr1.ovpn
+#open "/Applications/OpenVPN Connect/OpenVPN #Connect.app/Contents/MacOS/OpenVPN Connect"
+#vpn connection via terminal - install openvpn with brew and run the below
+openvpn --config ~/Documents/Projects/GitHub_Vasileios/Collaborative-Live-coding/ovpns/usr1.ovpn
+
