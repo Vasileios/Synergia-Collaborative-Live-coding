fdef53f19113f268f9d328b34e869267b86a435c
vasilis <agios.mur@gmail.com>
2021-09-12 18:37:14 +0300

    update

--- shell_scripts/2_oscgroups_sclang.sh
+++ shell_scripts/2_oscgroups_sclang.sh
@@ -3,14 +3,14 @@
 #open vpn and connect to the server
 #set correct path for /oscgroups/bin
 
-osascript -e 'tell app "Terminal"
-do script "openvpn --config ~/Collaborative-Live-coding/ovpns/usr1.ovpn"
-end tell' &&
+# osascript -e 'tell app "Terminal"
+# do script "openvpn --config ~/Collaborative-Live-coding/ovpns/usr1.ovpn"
+# end tell' &&
 
 osascript -e 'tell app "Terminal"
 do script "cd ~/Collaborative-Live-coding/ && ./bin/OscGroupClient 192.168.1.51 22242 22243 22244 22245 vas 1234 sccolgroup sccolpass"
-end tell'
+end tell0;95;0c'
 # run sclang on mac
-# osascript -e 'tell app "Terminal"
-#    do script "sh -c /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
-# end tell'
+ osascript -e 'tell app "Terminal"
+    do script "sh -c /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
+0;95;0c end tell'
