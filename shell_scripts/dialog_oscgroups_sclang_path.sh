42d7bbaf72de59020794be39bdfef6123f84b5c0
JasonSKK <ko00@windowslive.com>
2021-09-14 19:35:33 +0200

    READ_ME_WIN

--- shell_scripts/dialog_oscgroups_sclang_path.sh
+++ shell_scripts/dialog_oscgroups_sclang_path.sh
@@ -1,4 +1,9 @@
 #! /bin/sh
 
 #input OSCGroups & sclang path ==> then accessible as shell var #OSCGPATH & #SCLANGPATH
-OSCGPATH=`osascript -e 'set T to text returned of (display dialog "/OSCGRoupsInstall/oscgroups/bin/OscGroupClient fullpath?" buttons {"Cancel", "OK"} default button "OK" default answer "~/OSCGRoupsInstall/oscgroups/bin/OscGroupClient")'` && SCLANGPATH=`osascript -e 'set T to text returned of (display dialog "sclang executable fullpath?" buttons {"Cancel", "OK"} default button "OK" default answer "/Applications/SuperCollider.app/Contents/MacOS/sclang")'`
+OSCGPATH=`osascript -e 'set T to text returned of (display dialog "/OSCGRoupsInstall/oscgroups/bin/OscGroupClient fullpath?" buttons {"Cancel", "OK"} default button "OK" default answer "~/OSCGRoupsInstall/oscgroups/bin/OscGroupClient")'` &&
+
+    SCLANGPATH=`osascript -e 'set T to text returned of (display dialog "sclang executable fullpath?" buttons {"Cancel", "OK"} default button "OK" default answer "/Applications/SuperCollider.app/Contents/MacOS/sclang")'` &&
+osascript -e 'tell app "Terminal"
+do script "cd $OSCGPATH"
+end tell'
