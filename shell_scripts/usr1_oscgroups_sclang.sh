291d39fdaade67dcaf144f99e34d76d40474c982
vasilis <agios.mur@gmail.com>
2021-09-18 18:02:54 +0300

    update scripts

--- shell_scripts/usr1_oscgroups_sclang.sh
+++ shell_scripts/usr1_oscgroups_sclang.sh
@@ -1,9 +1,24 @@
-#! /bin/sh
+#run oscgroups and sclang for usr2
+
 osascript -e 'tell app "Terminal"
 do script "cd $(find Collaborative-Live-coding -type d | head -1) && ./bin/OscGroupClient 192.168.1.51 22242 22243 22244 22245 usr1 1234 sccolgroup sccolpass"
 end tell'
 
 # run sclang on mac
- osascript -e 'tell app "Terminal"
-    do script "sh -c /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
- end tell'
+# osascript -e 'tell app "Terminal"
+#    do script "sh -c #/Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
+# end tell'
+
+#run sclang on mac.. sclang dir
+#osascript -e 'tell app "Terminal"
+#do script "sh -c
+
+if [ -d "/Applications/SuperCollider/SuperCollider.app/Contents/MacOS/" ]
+then
+echo "Directory /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/ exists."
+
+"/Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
+
+else
+"/Applications/SuperCollider.app/Contents/MacOS/sclang"
+fi
