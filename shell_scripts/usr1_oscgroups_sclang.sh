35ade3d077bb80cc25823f164b19eb801215e176
vasilis <agios.mur@gmail.com>
2021-09-18 16:09:45 +0300

    update

---
+++ shell_scripts/usr1_oscgroups_sclang.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+osascript -e 'tell app "Terminal"
+do script "cd $(find Collaborative-Live-coding -type d | head -1) && ./bin/OscGroupClient 192.168.1.51 22242 22243 22244 22245 usr1 1234 sccolgroup sccolpass"
+end tell'
+
+# run sclang on mac
+ osascript -e 'tell app "Terminal"
+    do script "sh -c /Applications/SuperCollider/SuperCollider.app/Contents/MacOS/sclang"
+ end tell'
